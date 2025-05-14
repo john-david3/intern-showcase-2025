75dc2c04e431d93f134d5f7a06c135bccd7b154e
John David White <joh.white@f5.com>
2025-05-14 16:54:31 +0100

    skeleton code

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,12 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    uid INTEGER PRIMARY KEY,
+    uid INTEGER PRIMARY KEY AUTOINCREMENT,
     email VARCHAR(128),
     password VARCHAR(256),
+    firstname VARCHAR(32),
+    lastname VARCHAR(32),
     location VARCHAR(64)
 );
 
-INSERT INTO users (email, password, location)
-VALUES ('admin', 'admin', 'admin');
+INSERT INTO users (email, password, firstname, lastname, location)
+VALUES ('admin', 'admin', 'admin', 'admin', 'admin');
--- utils/sanitise.go
+++ utils/sanitise.go
@@ -1,1 +1,8 @@
 package utils
+
+import "strings"
+
+func sanitise(input string) string {
+	res := strings.TrimSpace(input)
+	return res
+}
