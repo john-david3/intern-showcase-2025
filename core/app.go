7049c6ed1197acddf2e03e8b98f6416c7f2342c3
John David White <127981157+john-david3@users.noreply.github.com>
2025-05-14 16:57:36 +0100

    Auth api (#5)
    * Setup backend server
    
    * Reinstall vite dependencies
    
    * Skeleton auth code
    
    * skeleton code

--- core/app.go
+++ core/app.go
@@ -6,6 +6,11 @@ import (
 )
 
 func Run() {
+	// Authentication routes
+	http.HandleFunc("/api/signup", signup)
+	http.HandleFunc("/api/login", login)
+	http.HandleFunc("/api/logout", logout)
+
 	slog.Info("server running on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		slog.Error("internal server error: ", "error", err)
--- core/auth.go
+++ core/auth.go
@@ -1,5 +1,15 @@
 package core
 
-func signup() {
+import "net/http"
+
+func signup(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
 
 }
